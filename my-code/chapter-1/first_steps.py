caed48034a1273897b1cce302a55ea3ed391f30d
Colin Paul <colin.paul@dell.com>
2018-02-13 16:46:20 +0000

    starting ds from scratch

--- my-code/chapter-1/first_steps.py
+++ my-code/chapter-1/first_steps.py
@@ -11,4 +11,14 @@ users = [
     { "id": 9, "name": "Klein" }
 ]
 
-print(users)
+# Friendships as derived from associations between user ids
+
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+for user in users:
+    user["friends"] = []
+
+for i, j in friendships:
+    users[i]["friends"].append(users[j])  # add j as a friend of i
+    users[j]["friends"].append(users[i])  # add i as a friend of j
